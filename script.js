f3f771ab2d25ea38b1b533a2aaf6b672cae07fe7
Shubham21117036 <b_sdinkar@me.iitr.ac.in>
2022-03-23 18:51:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,200 @@
+
+var audio = new Audio("file:///C:/music%20web/song/1.mp3");
+var giffy = document.getElementById('gif');
+var masterPlay = document.getElementById('playbutton');
+var barr = document.getElementById('bar');
+var theimage= document.getElementById('image');
+var heart1 = document.getElementById("heart1");
+var heart2 = document.getElementById("heart2");
+var heart3 = document.getElementById("heart3");
+var heart4 = document.getElementById("heart4");
+var heart5 = document.getElementById("heart5");
+let songs=[
+  {songname:"Agar Tum Saath Ho",filepath:"03 Tum Saath Ho - Tamasha (Arijit Singh) 320Kbps.mp3",coverpath:"agar tum saath ho.jpg"},
+  {songname:"Tum Ho",filepath:"22   Tum Ho (Rockstar).mp3",coverpath:"tum ho.jpg"},
+  {songname:"Tum Ho Toh",filepath:"ro.mp3",coverpath:"rock on.webp"},
+  {songname:"Aaoge Tum Kabhi",filepath:"128-Aaoge Tum Kabhi - Angry Indian Goddesses 128 Kbps.mp3",coverpath:"the local train.jpg"},
+  {songname:"Balam Pichkari",filepath:"Balam Pichkari (Full Song).mp3",coverpath:"balam pichkari.jpg"}
+
+]
+masterPlay.addEventListener('click',()=>{
+if(audio.paused)
+{ audio.play();
+  masterPlay.classList.remove('fa-circle-play');
+  masterPlay.classList.add('fa-circle-pause');
+  giffy.style.opacity=1;
+}
+else{
+  audio.pause();
+  masterPlay.classList.remove('fa-circle-pause');
+  masterPlay.classList.add('fa-circle-play');
+  giffy.style.opacity=0;
+}
+
+})
+audio.ontimeupdate= function(){
+var progress = (audio.currentTime/audio.duration)*100;
+barr.max = "100";
+barr.value = progress;
+}
+barr.onchange = function(){
+audio.currentTime = (barr.value*audio.duration/100);
+
+}
+var firstsong = document.getElementById("1");
+var secondsong = document.getElementById("2");
+var thirdsong = document.getElementById("3");
+var fourthsong = document.getElementById("4");
+var fifthsong = document.getElementById("5");
+var nameofsong = document.getElementById("name1")
+var nameofartist = document.getElementById("artist1");
+
+firstsong.onclick = function(){
+theimage.src = "file:///C:/music%20web/cover/1.jpg";
+audio.src = "file:///C:/music%20web/song/1.mp3";
+barr.value = 0;
+nameofsong.innerText = "Agar Tum Saath Ho";
+nameofartist.innerText = "Alka Yagnik,Arjit Singh";
+if(audio.paused)
+{ audio.play();
+  masterPlay.classList.remove('fa-circle-play');
+  masterPlay.classList.add('fa-circle-pause');
+  giffy.style.opacity=1;
+  
+}
+else{
+  audio.pause();
+  masterPlay.classList.remove('fa-circle-pause');
+  masterPlay.classList.add('fa-circle-play');
+  giffy.style.opacity=0;
+}
+
+}
+secondsong.onclick = function(){
+  theimage.src = "file:///C:/music%20web/cover/2.jpg";
+  audio.src = "file:///C:/music%20web/song/2.mp3";
+  barr.value=0;
+  nameofsong.innerText = "Tum Ho";
+  nameofartist.innerText = "Mohit Chauhan,Suzanne D'Mello";
+  if(audio.paused)
+  { audio.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+    giffy.style.opacity=1;
+  }
+  else{
+    audio.pause();
+    masterPlay.classList.remove('fa-circle-pause');
+    masterPlay.classList.add('fa-circle-play');
+    giffy.style.opacity=0;
+  } 
+  }
+  thirdsong.onclick = function(){
+    theimage.src = "file:///C:/music%20web/cover/3.jpg";
+    audio.src = "file:///C:/music%20web/song/3.mp3";
+    nameofsong.innerText = " Tum Ho Toh";
+    nameofartist.innerText = "Farhan Akhtar";
+    if(audio.paused)
+    { audio.play();
+      masterPlay.classList.remove('fa-circle-play');
+      masterPlay.classList.add('fa-circle-pause');
+      giffy.style.opacity=1;
+    }
+    else{
+      audio.pause();
+      masterPlay.classList.remove('fa-circle-pause');
+      masterPlay.classList.add('fa-circle-play');
+      giffy.style.opacity=0;
+    }
+    
+    }
+    fourthsong.onclick = function(){
+      theimage.src = "file:///C:/music%20web/cover/4.jpg";
+      audio.src = "file:///C:/music%20web/song/4.mp3";
+      nameofsong.innerText = "Aaoge Tum Kabhi";
+      nameofartist.innerText = "The Local Train";
+      if(audio.paused)
+      { audio.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        giffy.style.opacity=1;
+      }
+      else{
+        audio.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        giffy.style.opacity=0;
+      }
+    }
+      
+      
+      fifthsong.onclick = function(){
+        theimage.src = "file:///C:/music%20web/cover/5.jpg";
+        audio.src = "file:///C:/music%20web/song/5.mp3";
+        nameofsong.innerText = "Balam Pichkari";
+        nameofartist.innerText = "Vishal Dadlani,Shalmali Kholgade";
+        if(audio.paused)
+        { audio.play();
+          masterPlay.classList.remove('fa-circle-play');
+          masterPlay.classList.add('fa-circle-pause');
+          giffy.style.opacity=1;
+          }
+          else{
+            audio.pause();
+            masterPlay.classList.remove('fa-circle-pause');
+            masterPlay.classList.add('fa-circle-play');
+            giffy.style.opacity=0;
+          }
+        
+        }
+heart1.addEventListener("click",()=>{
+if(heart1.style.color!="red")
+  {heart1.style.color = "red";}
+else
+   {heart1.style.color="white";}
+
+})
+heart2.addEventListener("click",()=>{
+  if(heart2.style.color!="red")
+    {heart2.style.color = "red";}
+  else
+     {heart2.style.color="white";}
+  
+  })
+  heart3.addEventListener("click",()=>{
+    if(heart3.style.color!="red")
+      {heart3.style.color = "red";}
+    else
+       {heart3.style.color="white";}
+    
+    })
+    heart4.addEventListener("click",()=>{
+      if(heart4.style.color!="red")
+        {heart4.style.color = "red";}
+      else
+         {heart4.style.color="white";}
+      
+      })
+      heart5.addEventListener("click",()=>{
+        if(heart5.style.color!="red")
+          {heart5.style.color = "red";}
+        else
+           {heart5.style.color="white";}
+        
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
